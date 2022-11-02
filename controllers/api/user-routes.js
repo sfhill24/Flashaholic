89f191a65524a5b8ef92a950672c3cca98079811
Shelly Foxworth-Hill <sfhill24@gmail.com>
2022-11-01 22:13:25 -0400

    added middleware &  authentication routes for Login, Signup, and Logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,4 +27,79 @@ router.get("/", (req, res) => {
     });
 });
 
+//Signup route
+router.post("/signUp", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const user = await User.create({
+      username,
+      password,
+    });
+
+    delete user.password;
+
+    req.session.save(() => {
+      req.session.isAuthenticated = true;
+      req.session.currentUser = user;
+      res.status(200).json(user);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+
+//User login route
+router.post("/login", async (req, res) => {
+  const password = req.body.password;
+  const username = req.body.username;
+
+  try {
+    const user = await User.findOne({
+      where: { username },
+      attributes: { exclude: ['created_at, update_at'] },
+    });
+    if (!user) {
+      res.status(400).json({ message: "Incorrect email or password. Please try again!" })
+    }
+
+    const validPassword = await user.checkPassword(password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    delete user.password;
+
+    req.session.save(() => {
+      req.session.isAuthenticated = true;
+      req.session.currentUser = user;
+      res.status(200).json({ user, message: 'You are now logged in!' });
+    });
+
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+
+//Logout route
+router.post("/logout", (req, res) => {
+  if (req.session.isAuthenticated) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+})
+
+
+
 module.exports = router;
